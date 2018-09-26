45eaedd1a654d7e9998c263b03130eef58945116
NeverZoom <mrneverzoom@gmail.com>
2018-09-26 12:54:02 +0300

    lesson 2 all

--- test/lesson2/task1/Tests.kt
+++ test/lesson2/task1/Tests.kt
@@ -40,6 +40,7 @@ class Tests {
         assertEquals("12 лет", ageDescription(12))
         assertEquals("111 лет", ageDescription(111))
         assertEquals("199 лет", ageDescription(199))
+        assertEquals("109 лет", ageDescription(109))
     }
 
     @Test
@@ -57,6 +58,7 @@ class Tests {
         assertEquals(1, whichRookThreatens(5, 3, 7, 3, 4, 8))
         assertEquals(2, whichRookThreatens(6, 8, 8, 6, 6, 3))
         assertEquals(3, whichRookThreatens(3, 7, 8, 7, 3, 5))
+        assertEquals(0, whichRookThreatens(3, 7, 1, 5, 8, 9))
     }
 
     @Test
